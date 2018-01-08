d61097ebde7f0c5833f26357a57a95cf56711fa4
Nic <germanelectronix@gmail.com>
2018-01-08 21:25:26 +0100

    Remove unused import

--- Spigot/src/main/java/de/lenic/serverservice/spigot/server/resources/transformer/JsonTransformer.java
+++ Spigot/src/main/java/de/lenic/serverservice/spigot/server/resources/transformer/JsonTransformer.java
@@ -1,6 +1,5 @@
 package de.lenic.serverservice.spigot.server.resources.transformer;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import spark.ResponseTransformer;
@@ -10,10 +9,6 @@ import java.io.IOException;
 public class JsonTransformer implements ResponseTransformer {
 
     private static final ObjectMapper objectMapper = new ObjectMapper()
-            .configure(JsonParser.Feature.ALLOW_MISSING_VALUES, true)
-            .configure(JsonParser.Feature.ALLOW_TRAILING_COMMA, true)
-            .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true)
-            .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
             .configure(MapperFeature.USE_GETTERS_AS_SETTERS, false);
 
 
